b9515a2d423649ff8841394fb270933026af8338
natandiasm <natandiasm@gmail.com>
2024-03-03 15:08:25 -0300

    Added method for importing multiple movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,6 +22,26 @@ class MoviesController < ApplicationController
     end
   end
 
+  def import_form
+    # Renderize a view form
+  end
+
+  def import_csv
+    # Get csv from form
+    movies_csv = params[:movies_csv]
+    # Save CSV on temp folder
+    temp_path = helpers.save_file_temp_csv(movies_csv)
+    if helpers.csv_has_headers?(temp_path, ['title', 'director'])
+      # Call async job
+      job_movies = MoviesJob.perform_async(temp_path)
+
+      redirect_to movies_path, notice: 'Import has started, soon your movies will be imported.'
+    else
+      flash[:error] = "The CSV format sent is not accepted, send a file containing only the title and director of the movie."
+      redirect_to movies_path
+    end
+  end
+
   private
 
   def movie_params
